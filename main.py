4e0ec4dd694fdd81a2419eac1cf55f8aa4dd2b8f
comiker91 <mundt110@freenet.de>
2022-04-29 19:22:45 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+from multiprocessing import Pool
+
+# Settings
+# IP search range
+seach_ip = "192.168.2."
+# first ip (last ip section)
+seach_start = 0
+# last ip (last ip section)
+seach_end = 255
+# Process to use
+process = 10
+
+
+def check_ip(ip_end: int):
+    """Check a IP
+
+    Args:
+        ip_end (int): last ip section
+
+    Returns:
+        IP: return a IP if usable if not None
+    """
+    with open(os.devnull, "wb") as limbo:
+        ip = seach_ip+str(ip_end)
+        result = subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
+                                  stdout=limbo, stderr=limbo).wait()
+        if result:
+            return ip
+
+
+if __name__ == '__main__':
+    with Pool(processes=process) as pool:
+        check_list = pool.map(check_ip, range(seach_start, seach_end))
+
+    # Generate a list with free IPs
+    usable = list(ip for ip in check_list if ip)
+    # Maybe Save as file or something else
+    print(usable)
